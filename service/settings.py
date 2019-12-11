de2ec3dd6752fc810ee942bac2b67dd99fd945dc
dkmdebugin <salimonjamiu@yahoo.com>
2019-12-11 01:37:53 +0000

    Dump db data

--- service/settings.py
+++ service/settings.py
@@ -131,6 +131,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/report-attachments/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'report-attachments/')
