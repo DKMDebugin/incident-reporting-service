0f6ea981ff9d52cc33df13c8fea9d327fe7494ee
dkmdebugin <salimonjamiu@yahoo.com>
2020-04-21 14:47:44 +0100

    Final update for version 1

--- service/settings.py
+++ service/settings.py
@@ -79,23 +79,22 @@ WSGI_APPLICATION = 'service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': config("DB_NAME"),
-#        'USER': config("DB_USER"),
-#        'PASSWORD': config("DB_PASSWORD"),
-#        'HOST': config("DB_HOST"),
-#        'PORT': config("DB_PORT"),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': config("DB_NAME"),
+       'USER': config("DB_USER"),
+       'PASSWORD': config("DB_PASSWORD"),
+       'HOST': config("DB_HOST"),
+       'PORT': config("DB_PORT"),
     }
 }
-# mysql://reportingservice:@localhost:3306/incident.reporting.service
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
